8cfb2eb4b582bddbf6f2772d57e75c03ddc6150c
ghakan <nohtrim@gmail.com>
2014-10-29 08:54:00 +0100

    version 2

--- src/ProcessBuilderEjemplo.java
+++ src/ProcessBuilderEjemplo.java
@@ -6,51 +6,67 @@
 	public class ProcessBuilderEjemplo {
 		public static void main(String[] args) throws IOException, InterruptedException {
 
-		String seleccion;
+		char opcion;
+		String comando ="date";
 		
 		//Utilizamos Scanner y lo guardamos en la variable sc
 		Scanner sc = new Scanner(System.in);
 		
 		//Imprimimos en pantalla el texto
-		System.out.println("\nEscribe un comando. Ejemplo: (pwd), (ls), (whoami) o (Salir)");
+		System.out.println("\nEscribe un comando. Ejemplo: pwd(p), ls(l), whoami(w) o Salir(S)");
 		
 		//pedimos al usuario introducir lo solicitado y lo guardamos en la variable seleccion
-		seleccion = sc.next();
+		opcion = sc.next().charAt(0);
 
 		//si la variable cumple la condicion comienza el bucle
-		while (seleccion !=("salir") && seleccion !=("SALIR"))
+		while (opcion !=('s'))
 		{
 
-			//si la variable cumple con alguna de estas condiciones
-				if(seleccion == ("salir") || seleccion == ("SALIR") )
-				{
-					//este commando termina el bucle
-					break;
+			if (opcion == ('p') || opcion == ('l') || opcion == ('w')){
+				if(opcion == ('p') ){
+					comando="pwd";
+				}
+				else if(opcion == ('l')){
+
+						comando="ls";
+				}
+				else if(opcion == ('w')){
+
+					comando="whoami";
 				}
 
-			ProcessBuilder processBuilder = new ProcessBuilder(seleccion);
+				ProcessBuilder processBuilder = new ProcessBuilder(comando);
 
-			File dirOut = new File("/home/zubiri/ProyectosJava/resultado.txt");
-			File dirErr = new File("/home/zubiri/ProyectosJava/errores.txt");
+				File dirOut = new File("/home/zubiri/ProyectosJava/resultado.txt");
+				File dirErr = new File("/home/zubiri/ProyectosJava/errores.txt");
 
-			processBuilder.redirectOutput(dirOut);
-			processBuilder.redirectError(dirErr);
+				processBuilder.redirectOutput(dirOut);
+				processBuilder.redirectError(dirErr);
 
-			processBuilder.start();
+				processBuilder.start();
 
-			FileReader fr = new FileReader("/home/zubiri/ProyectosJava/resultado.txt");
+				FileReader fr = new FileReader("/home/zubiri/ProyectosJava/resultado.txt");
 
-			char [] mostrar = new char[100];
-            fr.read(mostrar);
-            System.out.println(mostrar);
-            fr.close();
+				BufferedReader br = new BufferedReader(fr); 
 
+	            String s; 
+	            while((s = br.readLine()) != null) 
+	            { 
+	                System.out.println(s);
+	            }
+	            fr.close();
+				//char [] mostrar = new char[100];
+	            //fr.read(mostrar);
+	            //System.out.println(s);
+	            //fr.close();
+	            
+        	}
 			//Si cumplio la condicion de C o R vuelve a preguntar que operacion quiere realizar
-			System.out.println("\n\nVuelve a escribir un comando. Ejemplo: (pwd), (ls), (whoami) o (Salir) \n");
+			System.out.println("\n\nVuelve a escribir un comando. Ejemplo: pwd(p), ls(l), whoami(w) o Salir(S) \n");
 			
-			seleccion = sc.next();
+			opcion = sc.next().charAt(0);
 		}
-
+		
 		//Cuando termina el bucle imprime en pantalla el texto.
 		System.out.println("\nHas seleccionado salir.\n");
 	}
